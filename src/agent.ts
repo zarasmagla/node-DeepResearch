94cb43a26a54f3b213812c81a099e8a1f5389c17
Han Xiao <han.xiao@jina.ai>
2025-04-15 23:28:44 +0800

    refactor: build ref with embeddings

--- src/agent.ts
+++ src/agent.ts
@@ -393,7 +393,7 @@ export async function getResponse(question?: string,
                                   badHostnames: string[] = [],
                                   onlyHostnames: string[] = [],
                                   maxRef: number = 10,
-                                  minRelScore: number = 0.7
+                                  minRelScore: number = 0.75
 ): Promise<{ result: StepAction; context: TrackerContext; visitedURLs: string[], readURLs: string[], allURLs: string[] }> {
 
   let step = 0;
